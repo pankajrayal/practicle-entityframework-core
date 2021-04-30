8bb8dd247acc3f7a202795fc7b083089caa0d86c
Pankaj Rayal <https://github.dxc.com/>
2021-04-30 10:14:10 +0530

    CH06-Data Access (Create, Read, Update, Delete)

---
+++ CH06/Activity0601_QuickCrud_Web/Activity0601_QuickCrud_Web/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Activity0601_QuickCrud_Web.Models {
+    public class Category {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
